161dd0634d1c5dcce2e49c125ee40cad1b2cd181
Jason Rigby <jason.rigby@monash.edu>
2018-12-18 15:47:43 +1100

    make exensions enables per-user

--- enable-instructor-tools.sh
+++ enable-instructor-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$JUPYTERHUB_USER" == course* ]]; then
-  jupyter nbextension enable --sys-prefix create_assignment/main
-  jupyter nbextension enable --sys-prefix formgrader/main --section=tree
-  jupyter serverextension enable --sys-prefix nbgrader.server_extensions.formgrader
+  jupyter nbextension enable --user create_assignment/main
+  jupyter nbextension enable --user formgrader/main --section=tree
+  jupyter serverextension enable --user nbgrader.server_extensions.formgrader
 fi
